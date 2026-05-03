51f5de9aa4395a4cd511d14f59fc9377ef959d58
Fernando Ruan <fernandosingaporefuture@gmail.com>
2026-05-03 10:53:20 -0300

    Books improved

--- includes/Books.hpp
+++ includes/Books.hpp
@@ -3,21 +3,65 @@
 
 # include <iostream>
 # include <string>
+# include <vector>
+
+/*
+
+In this book class, I represent only the books with their system ID, title, author, category and more. Notice that there is no quantity here—everything is structured separately. My focus here is the book and its characteristics; if I need anything additional, this is where it will be added. A book is not a shelf, a shelf is not a table, just as a table is not a user—everything is organized into classes that communicate with each other.
+
+*/
 
 class Books {
 	private:
-		int	_id;
+		unsigned long long int	_id; // 0 to 18.446.744.073.709.551.615
+		unsigned short int	_numberOfPages; // 0 to 65535
+		unsigned short int	_volume;
+		unsigned short int	_year;
+		std::string	_isbn; // International Standard Book Number
 		std::string	_title;
 		std::string	_author;
-		int	_year;
+		std::string	_category;
+		std::string	_publisher;
+		std::string	_edition;
+		std::string	_language;
+		std::string	_description;
+		std::vector<std::string>	_keywords; // to find the book by keywords in the system by a collection
 		Book(void); // I do not intend to have empty initializors of Books
 	public:
-		Book(std::string title, std::string author, int year);
+		Book(int isbn, std::string title, std::string author, std::string category, std::string language);
 		~Book(void);
 
-		std::string getTitle(void) const;
-		std::string getAuthor(void) const;
-		int getYear(void) const;
+		// get each information about the book, getters are forbidden to modificate any data (const)
+
+		unsigned long long int	getId(void) const;
+		unsigned short int	getNumberOfPages(void) const;
+		short int	getVolume(void) const;
+		std::string	getISBN(void) const;
+		std::string	getTitle(void) const;
+		std::string	getAuthor(void) const;
+		std::string	getCategory(void) const;
+		std::string	getPublisher(void) const;
+		std::string	getEdition(void) const;
+		std::string	getLanguage(void) const;
+		std::string	getDescription(void) const;
+		std::vector<std::string>	getKeywords(void) const;
+		unsigned short int	getYear(void) const;
+		
+		// update each information
+
+		void	setId(unsigned long long int id);
+		void	setNumberOfPages(unsigned short int numberOfPages);
+		void	setVolume(unsigned short int volume);
+		void	setISBN(std::string isbn);
+		void	setTitle(std::string title);
+		void	setAuthor(std::string author);
+		void	setCategory(std::string category);
+		void	setPublisher(std::string publisher);
+		void	setEdition(std::string edition);
+		void	setLanguage(std::string language);
+		void	setDescription(std::string description);
+		void	setKeywords(std::vector<std::string> keywords);
+		void	insertNewKeyword(std::string keyword);
 }
 
 #endif /* BOOKS_HPP */
--- srcs/Books.cpp
+++ srcs/Books.cpp
@@ -1,6 +1,19 @@
 #include "../includes/Books.hpp"
 
-Books::Books(std::string title, std::string author, int year) : _title(title), _author(author), _year(year) {
+Books::Books(std::string title, std::string author, std::string category, std::string language) : 
+	_title(title), 
+	_author(author), 
+	_category(category), 
+	_language(language),
+	_isbn(""),
+	_publisher(""),
+	_edition(""),
+	_description(""),
+	_keywords(),
+	_year(0),
+	_numberOfPages(0),
+	_volume(0)
+{
 	cout << "Book " << title << " created" << std::endl;
 }
 
@@ -8,14 +21,98 @@ Books::~Books(void) {
 	cout << "Book destroyed" << std::endl;
 }
 
-std::string Books::getAuthor(void) const {
-	return this->_author;
+unsigned long long int	Books::getId(void) const {
+	return (this->_id);
 }
 
-std::string Books::getTitle(void) const {
-	return this->_title;
+unsigned short int	Books::getNumberOfPages(void) const {
+	return (this->_numberOfPages);
 }
 
-int	Books::getYear(void) const {
-	return this->_year;
+int	Books::getVolume(void) const {
+	return (this->_volume);
+}
+
+std::string	Books::getISBN(void) const {
+	return (this->_isbn);
+}
+
+std::string	Books::getTitle(void) const {
+	return (this->_title);
+}
+
+std::string	Books::getAuthor(void) const {
+	return (this->_author);
+}
+
+std::string	Books::getCategory(void) const {
+	return (this->_category);
+}
+
+std::string	Books::getPublisher(void) const {
+	return (this->_publisher);
+}
+
+std::string	Books::getEdition(void) const {
+	return (this->_edition);
+}
+
+std::string	Books::getLanguage(void) const {
+	return (this->_language);
+}
+
+std::string	Books::getDescription(void) const {
+	return (this->_description);
+}
+
+std::vector<std::string>	Books::getKeywords(void) const {
+	return (this->_keywords);
+}
+
+void	Books::setId(unsigned long long int id) {
+	this->_id = id;
+}
+
+void	Books::setNumberOfPages(unsigned short int numberOfPages) {
+	this->_numberOfPages = numberOfPages;
+}
+
+void	Books::setVolume(unsigned short int volume) {
+	this->_volume = volume;
+}
+
+void	Books::setISBN(std::string isbn) {
+	this->_isbn = isbn;
+}
+
+void	Books::setTitle(std::string title) {
+	this->_title = title;
+}
+
+void	Books::setAuthor(std::string author) {
+	this->_author = author;
+}
+
+void	Books::setCategory(std::string category) {
+	this->_category = category;
+}
+
+void	Books::setEdition(std::string edition) {
+	this->_edition = edition;
+}
+
+void	Books::setLanguage(std::string language) {
+	this->_language = language;
+}
+
+void	Books::setDescription(std::string description) {
+	this->_description = description;
+}
+
+void	Books::setKeywords(vector<std::string> keywords) {
+	this->_keywords = keywords;
+}
+
+void	Books::insertNewKeyword(std::string keyword) {
+	this->_keywords.push_back(keyword);
 }
